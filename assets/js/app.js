8115356d5bede826039de434069b2208aea6a867
VansiluKodikara <vansilu08@gmail.com>
2026-06-26 22:05:04 +0530

    New commits

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,4 +16,44 @@ button3.addEventListener("click", function(){
     window.location.href = "https://github.com/VansiluKodikara/ThogakadePracticeProject";
 });
 
-const button4
+const button4 = document.getElementById("bookrent-code-btn");
+
+button4.addEventListener("click", function(){
+    window.location.href = "https://github.com/VansiluKodikara/BookRentalManagementSystem";
+});
+
+const button5 = document.getElementById("weatherfinder-code-btn");
+
+button5.addEventListener("click", function(){
+    window.location.href = "https://github.com/VansiluKodikara/Weather-application";
+});
+
+const button6 = document.getElementById("tide-code-btn");
+
+button6.addEventListener("click", function(){
+    window.location.href = "https://github.com/VansiluKodikara/Tide-Pomodoro-Timer";
+});
+
+const button7 = document.getElementById("portfolio-btn");
+
+button7.addEventListener("click", function(){
+    window.location.href = "https://vansilukodikara.vercel.app";
+});
+
+const button8 = document.getElementById("email-btn");
+
+button8.addEventListener("click", function(){
+    window.location.href = "kodikaravansilu@gmail.com";
+});
+
+const button9 = document.getElementById("linkedin-btn");
+
+button9.addEventListener("click", function(){
+    window.location.href = "https://www.linkedin.com/in/vansilu-kodikara-a3124b372/";
+});
+
+const button10 = document.getElementById("github-btn");
+
+button10.addEventListener("click", function(){
+    window.location.href = "https://github.com/VansiluKodikara";
+});
